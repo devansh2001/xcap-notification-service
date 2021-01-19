036536d9d8ba19febd81c3af5ed5fc6abe785682
devansh2001 <dponda3@gatech.edu>
2021-01-19 14:11:27 -0500

    removed logs

--- api.py
+++ api.py
@@ -32,7 +32,7 @@ def firebase_cloud_messaging_notification(device_id):
 
     response = requests.request("POST", url, headers=headers, data=body)
 
-    print(response.text)
+    # print(response.text)
 
     return response
 
@@ -52,13 +52,13 @@ def schedule_notification():
     if input_date_time is None or device_id is None:
         return 'Invalid'
     
-    print(input_date_time)
-    print(device_id)
+    # print(input_date_time)
+    # print(device_id)
 
     # https://stackabuse.com/converting-strings-to-datetime-in-python/ 
     notification_time = datetime.strptime(input_date_time, '%Y-%m-%d %H:%M:%S')
-    print(notification_time)
+    # print(notification_time)
 
     scheduler.add_job(firebase_cloud_messaging_notification, 'date', run_date=notification_time, args=[device_id])
-    print('Scheduled!')
+    # print('Scheduled!')
     return 'OK'
